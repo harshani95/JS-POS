c06d67973a711229a484ea252da8a42e6ca58292
harshani <ruwanthikapemarathna@gmail.com>
2024-09-27 20:33:48 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCG5FBOJSsnhemfi8GL7pKjSH9DUcJ3KOg",
+    authDomain: "js-pos-app.firebaseapp.com",
+    projectId: "js-pos-app",
+    storageBucket: "js-pos-app.appspot.com",
+    messagingSenderId: "212839450060",
+    appId: "1:212839450060:web:0e7dfbfebf6486216ac969",
+    measurementId: "G-R9MGF5TZCE"
+  };
